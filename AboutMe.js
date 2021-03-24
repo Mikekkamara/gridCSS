7839d8ce00f0c735c1a734eff97056c39ffb5fe7
Mikekkamara <mikekkamara@gmail.com>
2021-03-25 00:47:13 +0300

    First Commit

---
+++ AboutMe.js
@@ -0,0 +1,20 @@
+
+function  hey(){
+
+    var obj = new Date();
+    var hrs= obj.getHours();
+    var mins = obj.getMinutes();
+    var sec = obj.getSeconds();
+    if(mins<=9){
+        document.getElementById("date").innerHTML= hrs+":"+0+mins+":"+sec;
+    }
+    if(sec<=9){
+        document.getElementById("date").innerHTML= hrs+":"+mins+":"+0+sec;
+    }
+    if(hrs<=9){
+        document.getElementById("date").innerHTML= 0+hrs+":"+mins+":"+sec;
+    }
+    document.getElementById("date").innerHTML= hrs+":"+mins+":"+sec;
+    
+}
+setInterval(hey, 1000);
